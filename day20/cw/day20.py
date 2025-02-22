104138e0e78445c2efd7982f3ff997ea064f87fc
anamaria013 <topuriateo11@gmail.com>
2025-02-22 15:08:20 +0400

    commit

---
+++ day20/cw/day20.py
@@ -0,0 +1,8 @@
+age=int(input("enter:  "))
+
+if age == 10:
+    ("same")
+elif  age < 10:
+    ("not same")
+else:
+    ("not right")
---
+++ day20/hw/day20.py
@@ -0,0 +1,30 @@
+num=10
+for i in range(0,num+1):
+    if num%2==0:
+        print(str(num)+" is even")
+    else:
+        print(str(num)+" is odd")
+
+print(10_10)
+name1="anamaria"
+name2=input("enter your name :  ")
+for i in range(0,num+1):
+    if name1==name2:
+        print(str(name1)+" hello")
+    else:
+        print(str(name2)+" buye")
+sum=int(input("enter sum:  "))
+for i in range(0,sum+1):
+    if sum>90:
+        print(str(sum)+" a")
+
+    else:
+        print(str(sum)+"b")
+
+while 1<100:
+    if 12%100:
+        print(str(sum)+" კენტი")
+
+    else:
+        print(str(sum)+"ლუწი")
+        sum=+1
