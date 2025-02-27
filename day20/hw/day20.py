dea274855fcf9c9ebd4f7c3c7c66a3f0f3ed858c
anamaria013 <topuriateo11@gmail.com>
2025-02-27 23:20:25 +0400

    commit

--- day20/hw/day20.py
+++ day20/hw/day20.py
@@ -1,14 +1,18 @@
-num=10
-for i in range(0,num+1):
-    if num%2==0:
-        print(str(num)+" is even")
+num0=input("enter your num0 :  ")
+for i in num0:
+    print(i)
+
+sumber4=10
+for i in range(0,sumber4+1):
+    if sumber4%2==0:
+        print(str(sumber4)+" is even")
     else:
-        print(str(num)+" is odd")
+        print(str(sumber4)+" is odd")
 
 print(10_10)
 name1="anamaria"
 name2=input("enter your name :  ")
-for i in range(0,num+1):
+for i in name2:
     if name1==name2:
         print(str(name1)+" hello")
     else:
