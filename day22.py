c83a7417653deb24fb87095ec45dfa5f73e0c2a5
anamaria013 <topuriateo11@gmail.com>
2025-03-12 17:01:20 +0400

    commit

---
+++ day22.py
@@ -0,0 +1,11 @@
+# elif-elseif
+age=3
+if age< 18  :
+    print("cilde")
+print ("happy")
+film="chao bella"
+filma=input("enter film")
+if film==filma:
+    print("our movie taste is similar")
+elif film<filma:
+    print("we like different movies")
