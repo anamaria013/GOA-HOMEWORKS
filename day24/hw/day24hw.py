ac7d8c92615c63c7a265517f16b0247522783f10
anamaria013 <topuriateo11@gmail.com>
2025-03-21 20:45:23 +0400

    commit

---
+++ day24/hw/day24hw.py
@@ -0,0 +1,9 @@
+food=["ხინკალი" , "ხაჭაპური" , "პიცა"]
+print(food)
+comp=["ცომპიუტერი", "მაუსი"]
+print(0 , comp)
+py=["print" "print", "if" "if", "else""else" , "elif""elif" , "while""while" ,  "for""for" ,  "true""true" , "false""false"]
+print(py)
+
+py=["print" , "if" , "else" , "elif" , "while" ,  "for" ,  "true" , "false"]
+print(-1 , py)
