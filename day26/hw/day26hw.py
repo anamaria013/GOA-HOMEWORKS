7120d003ddf99f9f65bf802574db639e44ad3193
anamaria013 <topuriateo11@gmail.com>
2025-04-09 10:27:13 +0400

    commit

---
+++ day26/hw/day26hw.py
@@ -0,0 +1,9 @@
+num=[1,2,3,4,5,6,7,8,9,10.]
+num=num[0:5]
+print(num)
+num1=[20,21,22,23,24,25,26,27,28,29,30]
+num1=num1[ : :2 ]
+print (num1)
+name="Goal-Oriented Academy"
+name=name[14:21]
+print(name)
