3f50869538f2aac650321d50291b830c9b687113
anamaria013 <topuriateo11@gmail.com>
2025-04-15 12:37:59 +0400

    commit

---
+++ day28/cw/day28.py
@@ -0,0 +1,10 @@
+# Prompt the user to enter their name
+name = input("Enter your name: ")
+
+# Check if the first letter of the name is uppercase
+if name[0].isupper():
+    print("Hello!")
+else:
+    print("Bye...")
+
+
