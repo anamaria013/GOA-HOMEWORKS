a8476862538a2740dcac0499dc219429dfea2067
anamaria013 <topuriateo11@gmail.com>
2025-05-02 19:13:33 +0400

    commit

---
+++ day30.5/cw/day30.5.py
@@ -0,0 +1,12 @@
+
+my_name = "anamaria"  
+
+# Ask the user for their name
+user_name = input("Please enter your name: ")
+
+# Compare the names (case insensitive)
+if my_name.lower() == user_name.lower():
+    print("Our names are similar!")
+else:
+    print("We have different names.")
+
