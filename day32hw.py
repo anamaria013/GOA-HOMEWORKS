8a7ce8ed769019f447c3a155f44c9c07ba081fc5
anamaria013 <topuriateo11@gmail.com>
2025-05-24 12:29:29 +0400

    commit

---
+++ day32hw.py
@@ -0,0 +1,15 @@
+def sum_of_three(a, b, c, d):
+    
+    return a + b + c
+
+def user_info(name, age):
+    
+    return f"{name} is {age} წლის."
+
+def square_number(num):
+     
+    return num ** 2
+
+def power_number(base, exponent):
+    
+    return base ** exponent
